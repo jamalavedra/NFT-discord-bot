d2a0ca65e7c667b1d18d902a30afc4e7efeb6937
jamalavedra <jamalavedra@gmail.com>
2022-05-17 14:29:35 +0200

    working version

--- cronjobs/listing.js
+++ cronjobs/listing.js
@@ -12,11 +12,11 @@ module.exports = {
   enabled: process.env.DISCORD_LISTING_CHANNEL_ID != null,
   async execute(client) {
     if (lastTimestamp == null) {
-      lastTimestamp = Math.floor(Date.now() / 1000) - 120;
+      lastTimestamp = Math.floor(Date.now() / 1000) - 7200;
     } else {
-      lastTimestamp -= 30;
+      lastTimestamp -= 7230;
     }
-    let newTimestamp = Math.floor(Date.now() / 1000) - 30;
+    let newTimestamp = Math.floor(Date.now() / 1000);
     // we're retrieving events from -90 to -30 seconds ago each time, and each query overlaps the previous query by 30 seconds
     // doing this to try to resolve some intermittent issues with events being missed by the bot, suspect it's due to OpenSea api being slow to update the events data
     // duplicate events are filtered out by the listingCache array
@@ -36,7 +36,7 @@ module.exports = {
     do {
       let url = `${openseaEventsUrl}?collection_slug=${
         process.env.OPEN_SEA_COLLECTION_NAME
-      }&event_type=created&only_opensea=false&occurred_before=${newTimestamp}${
+      }&event_type=transfer&only_opensea=false&occurred_before=${newTimestamp}${
         next == null ? "" : `&cursor=${next}`
       }`;
       try {
@@ -46,19 +46,10 @@ module.exports = {
         }
 
         let data = await res.json();
-
         next = data.next;
 
         data.asset_events.forEach(function (event) {
-          if (
-            event.asset &&
-            event.asset.description &&
-            (event.asset.description
-              .toLowerCase()
-              .includes("#longlifenation") ||
-              event.asset.description.toLowerCase().includes("#pow") ||
-              event.asset.description.toLowerCase().includes("#lnn"))
-          ) {
+          if (event.asset) {
             if (listingCache.includes(event.id)) {
               newEvents = false;
               return;
@@ -71,30 +62,38 @@ module.exports = {
               newEvents = false;
               return;
             }
+            if (
+              event.asset.description &&
+              (event.asset.description
+                .toLowerCase()
+                .includes("#longlifenation") ||
+                event.asset.description.toLowerCase().includes("#pow") ||
+                event.asset.description.toLowerCase().includes("#lnn"))
+            ) {
+              const embedMsg = new Discord.MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle(event.asset.name)
+                .setURL(event.asset.permalink)
+                .setDescription(`${event.asset.description}`)
+                .setThumbnail(event.asset.image_url)
+                .addField(
+                  "By",
+                  `[${event.transaction.to_account.address.slice(
+                    0,
+                    8
+                  )}](https://polygonscan.com/address/${
+                    event.transaction.to_account.address
+                  })`,
+                  true
+                );
 
-            const embedMsg = new Discord.MessageEmbed()
-              .setColor("#0099ff")
-              .setTitle(event.asset.name)
-              .setURL(event.asset.permalink)
-              .setDescription(
-                `has just been listed for ${event.starting_price / 1e18}\u039E`
-              )
-              .setThumbnail(event.asset.image_url)
-              .addField(
-                "By",
-                `[${
-                  event.seller.user?.username ||
-                  event.seller.address.slice(0, 8)
-                }](https://etherscan.io/address/${event.seller.address})`,
-                true
-              );
-
-            client.channels
-              .fetch(process.env.DISCORD_LISTING_CHANNEL_ID)
-              .then((channel) => {
-                channel.send(embedMsg);
-              })
-              .catch(console.error);
+              client.channels
+                .fetch(process.env.DISCORD_LISTING_CHANNEL_ID)
+                .then((channel) => {
+                  channel.send(embedMsg);
+                })
+                .catch(console.error);
+            }
           }
         });
       } catch (error) {
